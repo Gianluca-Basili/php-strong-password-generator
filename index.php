9109facb61ace1fb32a11822475bd103a29674bb
Gianluca-basili <gianluc.basili@gmail.com>
2023-07-06 15:10:52 +0200

    check

---
+++ index.php
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <form action="index.php" method="get">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <label for="length" class="control-label">Lunghezza</label>
+                            <input type="number" name="length" id="length" placeholder="Lunghezza" class="form-control">
+                            <div class="col-12 col-md-6">
+                                <button type="submit" class="btn btn-success float-end mt-4">Invia</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
